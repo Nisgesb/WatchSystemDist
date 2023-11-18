148b21b1554aec963434ff3358b8809ef6176434
zs <2546480616@qq.com>
2023-11-19 00:29:18 +0800

    得知jsdelivr不能托管压缩过的文件，将压缩文件还原，将文件体积减小

---
+++ assets/404-02e34a3e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-ebc2234f.js";const c={};function n(r,t){return Vue.openBlock(),Vue.createElementBlock("h1",null,"404页面")}const _=e(c,[["render",n]]);export{_ as default};
--- assets/404-76b100b7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-77cc70b0.js";const c={};function n(r,t){return Vue.openBlock(),Vue.createElementBlock("h1",null,"404页面")}const _=e(c,[["render",n]]);export{_ as default};
